e23934329e8593bf7b26e742453c1044476814c3
sohamgurav5305 <gurav.sohamsudhakar@gmail.com>
2026-05-05 02:16:11 +0530

    add prometheus monitoring

--- app/app.py
+++ app/app.py
@@ -2,9 +2,21 @@ from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, request, jsonify, render_template
 from sqlalchemy import text
 from sqlalchemy.exc import IntegrityError
+from prometheus_client import start_http_server, Counter
 import time
 
 app = Flask(__name__)
+
+REQUEST_COUNT = Counter('request_count', 'Total Requests')
+
+@app.route('/')
+def home():
+    REQUEST_COUNT.inc()
+    return "LoanFlow Running 🚀"
+
+if __name__ == "__main__":
+    start_http_server(8000)
+    app.run(host='0.0.0.0', port=5000)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///loanflow.db'
 db = SQLAlchemy(app)
